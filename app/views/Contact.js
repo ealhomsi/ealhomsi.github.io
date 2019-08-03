ace027710be453cfacc01feea77a074e353ace33
Elias Al Homsi <elias.alhomsi@mail.mcgill.ca>
2019-08-03 01:11:33 -0700

    add shopify store

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -110,6 +110,7 @@ const Contact = () => {
         </ul>
         
         <p><a href="/resume.pdf">download my <strong>resume.pdf</strong></a></p>
+        <p><a href="shopify.eliashomsi.com"> <i>bonus:</i> checkout my cool tshirts website</a></p>
       </article>
     </Main>
   );
