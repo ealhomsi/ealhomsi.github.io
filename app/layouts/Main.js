db3b0df9af02849f5b9ec9c62b3b77f43a09602a
Elias Al Homsi <elias.alhomsi@mail.mcgill.ca>
2020-04-12 23:03:34 -0400

    remove react-ga

--- app/layouts/Main.js
+++ app/layouts/Main.js
@@ -2,27 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import ReactGA from 'react-ga';
-
 import Header from '../components/Template/Header';
 import Nav from '../components/Template/Nav';
-
-if (NODE_ENV === 'production') {
-  ReactGA.initialize(GA_ID);
-}
 
 class Main extends Component {
   ComponentDidMount() {
     window.scrollTo(0, 0);
-  }
-
-  componentDidMount() {
-    if (NODE_ENV === 'production') {
-      ReactGA.set({
-        page: window.location.pathname,
-      });
-      ReactGA.pageview(window.location.pathname);
-    }
   }
 
   render() {
